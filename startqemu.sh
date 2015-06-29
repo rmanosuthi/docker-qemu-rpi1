ea87a4177d9ea1d803c14d6e45410907d682db87
mpipo <themelo64@gmail.com>
2015-06-29 22:39:52 +0700

    Removed ADDITIONAL_QEMUPARAMS
    There's (probably) a bug in QEMU which if an empty variable is in the
    command, it will derp a lot.

--- startqemu.sh
+++ startqemu.sh
@@ -12,8 +12,20 @@ detect_img_qcow2() {
 	find /qemu/hostvol|xargs file|grep "QEMU QCOW Image" > /tmp/dt_img_qcow2
 }
 
+start_qemu_qcow2() {
+	qemu-system-arm -M versatilepb -cpu arm1176 -m 256 -hda /qemu/hostvol/os.qcow2 -kernel /qemu/kernel-qemu -append "root=$APPENDED_ROOTDEVICE $ADDITIONAL_BOOTPARAMS" -vnc 127.0.0.1:1
+}
+
+start_qemu_img() {
+	qemu-system-arm -M versatilepb -cpu arm1176 -m 256 -hda /qemu/hostvol/os.img -kernel /qemu/kernel-qemu -append "root=$APPENDED_ROOTDEVICE $ADDITIONAL_BOOTPARAMS" -vnc 127.0.0.1:1
+}
+
 start_qemu() {
-	qemu-system-arm -M versatilepb -cpu arm1176 -m 256 -hda /qemu/hostvol/os.qcow2 -kernel /qemu/kernel-qemu -append "root=$APPENDED_ROOTDEVICE $ADDITIONAL_BOOTPARAMS" -vnc 127.0.0.1:1 "$ADDITIONAL_QEMUPARAMS"
+	if [ -f /qemu/hostvol/os.img ]; then
+		start_qemu_img
+	else
+		start_qemu_qcow2
+	fi
 }
 
 start_qemu
