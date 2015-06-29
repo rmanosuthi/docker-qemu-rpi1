2e69d50c803940fe6be3e86f6248be6cf85a892a
mpipo <themelo64@gmail.com>
2015-06-29 22:21:55 +0700

    Added option for additional qemu parameters

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --net host -e APPENDED_ROOTDEVICE="/dev/sda" -e ADDITIONAL_BOOTPARAMS="" -p 5901:5901 -d -v "$MOUNT":/qemu/hostvol/ --name qemu-rpi mpipo/qemu-rpi1
+docker run --net host -e APPENDED_ROOTDEVICE="/dev/sda" -e ADDITIONAL_BOOTPARAMS="" -e ADDITIONAL_QEMUPARAMS="" -p 5901:5901 -d -v "$MOUNT":/qemu/hostvol/ --name qemu-rpi mpipo/qemu-rpi1
