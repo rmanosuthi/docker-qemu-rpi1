85bcfdf4f6eb830c0f1c9a334fa6ef9e61da3322
mpipo <themelo64@gmail.com>
2015-07-04 20:28:18 +0700

    Functions cleanup

--- startqemu.sh
+++ startqemu.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Unused functions
+
 convert_raw_to_qcow2() {
 	qemu-img -f raw -O qcow2 /qemu/hostvol/"$DETECTED_SYSIMG" /qemu/hostvol/"$DETECTED_SYSIMG".qcow2
 }
@@ -12,12 +14,35 @@ detect_img_qcow2() {
 	find /qemu/hostvol|xargs file|grep "QEMU QCOW Image" > /tmp/dt_img_qcow2
 }
 
+# Used functions
+
+verify_if_image_exists_img() {
+	if [ -f /qemu/hostvol/os.img ]; then
+		:
+	else
+		echo "Image not found, exiting."
+	exit 1
+}
+
+verify_if_image_exists_qcow2() {
+	if [ -f /qemu/hostvol/qcow2.img ]; then
+		:
+	else
+		echo "Image not found, exiting."
+	exit 1
+}
+
+vfimage() {
+	verify_if_image_exists_img
+	verify_if_image_exists_qcow2
+}
+
 start_qemu_qcow2() {
-	qemu-system-arm -M versatilepb -cpu arm1176 -m 256 -hda /qemu/hostvol/os.qcow2 -kernel /qemu/kernel-qemu -append "root=$APPENDED_ROOTDEVICE $ADDITIONAL_BOOTPARAMS" -vnc 127.0.0.1:1 -netdev tap,id=br0
+	qemu-system-arm -M versatilepb -cpu arm1176 -m 256 -hda /qemu/hostvol/os.qcow2 -kernel /qemu/kernel-qemu -append "root=$APPENDED_ROOTDEVICE $ADDITIONAL_BOOTPARAMS" -vnc 127.0.0.1:1 -netdev user
 }
 
 start_qemu_img() {
-	qemu-system-arm -M versatilepb -cpu arm1176 -m 256 -hda /qemu/hostvol/os.img -kernel /qemu/kernel-qemu -append "root=$APPENDED_ROOTDEVICE $ADDITIONAL_BOOTPARAMS" -vnc 127.0.0.1:1 -netdev tap,id=br0
+	qemu-system-arm -M versatilepb -cpu arm1176 -m 256 -hda /qemu/hostvol/os.img -kernel /qemu/kernel-qemu -append "root=$APPENDED_ROOTDEVICE $ADDITIONAL_BOOTPARAMS" -vnc 127.0.0.1:1 -netdev user
 }
 
 start_qemu() {
@@ -28,4 +53,5 @@ start_qemu() {
 	fi
 }
 
+vfimage
 start_qemu
